b174879e73a94ef40351ed9e09f106069c74085e
Pikul <pikcyk777@gmail.com>
2026-06-24 13:49:04 +0200

    Redesign with Kwazar colors and logo

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">🎳 Bowling Club</div>
+      <a href="/" className="navbar-logo">
+        <Image
+          src="/logo.png"
+          alt="Kwazar Bowling Club"
+          width={80}
+          height={80}
+          style={{objectFit: 'contain'}}
+        />
+      </a>
       <div className="nav-links">
         <a href="/">Strona główna</a>
         <a href="/about">O nas</a>
